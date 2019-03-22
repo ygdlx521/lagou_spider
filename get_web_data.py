268601c309d1be656ce8a208faa0c9e85bbc86bd
ygdlx521 <136658687@qq.com>
2019-03-22 17:26:38 +0800

    add code

--- get_web_data.py
+++ get_web_data.py
@@ -4,42 +4,42 @@ import time
 import random
 import pandas as pd
 
+# 爬取数据的字段名
 field_name = ['position_id', 'position_salary', 'position_name', 'position_url', 'position_city', 'position_area',
               'position_publishtime', 'work_year', 'education', 'position_desc', 'position_tags', 'hr_id',
               'company_name', 'company_id', 'company_url', 'company_desc']
+# 爬取的职位信息列表
+job_info_list = []
 
 
 def get_job_index(index_url):
     """
-    通过拉勾首页获取各职位对应名称及url
-    因为对首页只是一次访问即可获取，不做headers/cookies设置
-    """
-    r = requests.get(index_url)
-    soup_index = BeautifulSoup(r.text, 'lxml')
+    爬取职位总览：通过拉勾首页获取各职位的分类标签，职位名称，以及对应链接。例如：
+    ['技术', '后端开发', 'Python', 'https://www.lagou.com/zhaopin/Python/']
 
-    job_index_list = []  # 保存各职位名称+url
+    """
+    job_index_list = []  # 初始化列表，保存各职位总览数据
+    r = requests.get(index_url)  # 请求首页链接
+    soup_index = BeautifulSoup(r.text, 'lxml')  # 创建解析对象
+
     # '#'号开头的是通过html的id名查找，例如：#sidebar；直接写div是通过html的标签查找；'.'开头的是通过类名查找，例如：.menu_box。
     for each_first in soup_index.select('#sidebar > div > .menu_box'):
         first_name = each_first.select_one('h2').get_text().strip(' \n')  # 第一分类名称
-
         for each_second in each_first.select('div.menu_sub.dn > dl'):
             second_name = each_second.select_one('dt > span').get_text()  # 第二分类名称
-
             for each_third in each_second.select('dd > a'):
                 third_name = each_third.get_text()  # 职位名称
                 third_url = each_third['href']  # 职位url
-                # ['技术', '后端开发', 'Python', 'https://www.lagou.com/zhaopin/Python/']
                 job_index_list.append([first_name, second_name, third_name, third_url])
     return job_index_list
 
 
 def parse_position_page(html_page):
-    job_info_list = []
     bs_obj = BeautifulSoup(html_page, 'lxml')
     for each_position in bs_obj.select('#s_position_list > ul > li'):
         position_id = each_position['data-positionid']  # 职位id
         position_salary = each_position['data-salary']  # 薪资
-        position_name = each_position['data-positionname']  # 职位名称
+        position_name = each_position['data-positionname'].strip().replace(',', '，')  # 职位名称
         position_url = each_position.select_one('.position_link')['href']  # 职位url
         position_location = each_position.select_one('.add > em').get_text()  # 工作地点
         position_publishtime = each_position.select_one('.format-time').get_text()  # 发布时间
@@ -62,7 +62,9 @@ def parse_position_page(html_page):
             print(err)
             print(each_position)
             position_city = position_location
-            position_area = ''
+            position_area = ""
+            work_year = position_requ
+            education = ""
         job_info_list.append(
             [position_id, position_salary, position_name, position_url, position_city, position_area,
              position_publishtime,
@@ -73,14 +75,16 @@ def parse_position_page(html_page):
 
 def download_position_page(url_prefix, page_num):
     headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,'
+                  'application/signed-exchange;v=b3',
         'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'zh-CN,zh;q=0.9',
         'Cache-Control': 'max-age=0',
         'Connection': 'keep-alive',
         'Host': 'www.lagou.com',
         'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/73.0.3683.75 Safari/537.36 '
     }
 
     s = requests.Session()
@@ -91,79 +95,6 @@ def download_position_page(url_prefix, page_num):
     print(url_prefix + str(page_num) + '/')
     print(len(r.text))
     return r.text
-
-
-def get_position(position_list):
-    """
-    根据从首页获得的各职位对应url，对每个职位进行翻页保存职位信息
-    创建会话，每个职位有1次会话
-    """
-    position_info = []
-
-    for positions_url in [position_list[i][3] for i in range(len(position_list)) if position_list[i][2] == 'Python']:
-        headers = {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'zh-CN,zh;q=0.9',
-            'Cache-Control': 'max-age=0',
-            'Connection': 'keep-alive',
-            'Host': 'www.lagou.com',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'
-        # 每个职位，随机选择一个User-Agent
-        }
-
-        s = requests.Session()
-        s.headers.update(headers)  # 需要设置headers信息，否则返回登陆页面
-        s.get('http://www.lagou.com')  # 第一步先访问首页，获得cookies
-        time.sleep(1)
-
-        for pn in range(11, 21):
-            r = s.get(positions_url + str(pn) + '/')  # 循环进行翻页访问
-            print(positions_url + str(pn) + '/')
-            time.sleep(random.uniform(1, 3))
-            print(len(r.text))
-            # if len(r.text) < 2000: break  # 如果翻页完毕，退出当前职位爬取
-            print(pn)
-
-            soup_positon = BeautifulSoup(r.text, 'lxml')
-
-            for each_position in soup_positon.select('#s_position_list > ul > li'):
-                position_id = each_position['data-positionid']  # 职位id
-                position_salary = each_position['data-salary']  # 薪资
-                position_name = each_position['data-positionname']  # 职位名称
-                position_url = each_position.select_one('.position_link')['href']  # 职位url
-                position_location = each_position.select_one('.add > em').get_text()  # 工作地点
-                position_publishtime = each_position.select_one('.format-time').get_text()  # 发布时间
-                position_requ = \
-                    each_position.select_one('div.position > div.p_bot > div.li_b_l').get_text().split('\n')[-2]  # 职位要求
-                position_desc = each_position.select_one('div.li_b_r').get_text().replace(',', '，')  # 职位描述
-                position_tags = each_position.select_one('div.list_item_bot > div.li_b_l').get_text('/')  # 职位tags
-
-                hr_id = each_position['data-hrid']  # HRid
-                company_name = each_position['data-company']  # 供职公司
-                company_id = each_position['data-companyid']  # 公司id
-                company_url = each_position.select_one('.company_name > a')['href']  # 公司url
-                company_desc = each_position.select_one('div.industry').get_text().strip().replace(',', '，')  # 公司行业描述
-                try:
-                    position_city = position_location.split('·')[0]
-                    position_area = position_location.split('·')[1]
-                    work_year = position_requ.split('/')[0].strip()
-                    education = position_requ.split('/')[1].strip()
-                except IndexError as err:
-                    print(err)
-                    print(each_position)
-                    position_city = position_location
-                    position_area = ''
-                position_info.append(
-                    [position_id, position_salary, position_name, position_url, position_city, position_area,
-                     position_publishtime,
-                     work_year, education, position_desc, position_tags, hr_id, company_name, company_id, company_url,
-                     company_desc])
-            print(len(position_info))
-        # if len(position_info) >= 4000: break  # 爬取四千条数据即终止
-
-    return position_info
 
 
 def save_data(file_name, job_data):
@@ -179,15 +110,11 @@ def save_data(file_name, job_data):
                 print(err)
 
 
-def save_data_pd(file_name, job_data):
-    df = pd.DataFrame(data=job_data, columns=field_name)
-    df.to_csv(file_name, index=False)
-
-
 if __name__ == '__main__':
     job_index_list = get_job_index("http://www.lagou.com")
     for i in job_index_list:
-
         print(i)
-    html_page = download_position_page("https://www.lagou.com/zhaopin/Python/", 1)
-    parse_position_page(html_page)
+    for i in range(1, 31):
+        html_page = download_position_page("https://www.lagou.com/zhaopin/Python/", i)
+        parse_position_page(html_page)
+    save_data("out.csv", job_info_list)
--- job_analysis.py
+++ job_analysis.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import sm as sm
+import statsmodels.api as sm
 from wordcloud import WordCloud
 from scipy.misc import imread
 import jieba
@@ -9,119 +9,135 @@ from pylab import mpl
 # 使matplotlib模块能显示中文
 mpl.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体
 mpl.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
-
-# 读取数据
-df = pd.read_csv('out1.csv', encoding='gbk')
-print(df)
-
-# 数据清洗,剔除实习岗位
-df.drop(df[df['position_name'].str.contains('实习')].index, inplace=True)
-# print(df.describe())
-
-# 由于CSV文件内的数据是字符串形式,先用正则表达式将字符串转化为列表,再取区间的均值
-pattern = '\d+'
-df['position_experience'] = df['work_year'].str.findall(pattern)
-
-avg_work_year = []
-for i in df['position_experience']:
-    # 如果工作经验为'不限'或'应届毕业生',那么匹配值为空,工作年限为0
-    if len(i) == 0:
-        avg_work_year.append(0)
-    # 如果匹配值为一个数值,那么返回该数值
-    elif len(i) == 1:
-        avg_work_year.append(int(''.join(i)))
-    # 如果匹配值为一个区间,那么取平均值
-    else:
-        num_list = [int(j) for j in i]
-        avg_year = sum(num_list) / 2
-        avg_work_year.append(avg_year)
-
-df['经验'] = avg_work_year
-
-# 将字符串转化为列表,再取区间的前25%，比较贴近现实
-df['salary'] = df['position_salary'].str.findall(pattern)
-
 avg_salary = []
-for k in df['salary']:
-    int_list = [int(n) for n in k]
-    try:
-        avg_wage = int_list[0] + (int_list[1] - int_list[0]) / 4
-    except Exception as err:
-        print(err)
-        print(k)
-    avg_salary.append(avg_wage)
-
-df['月工资'] = avg_salary
-# 将清洗后的数据保存,以便检查
-df.to_csv('draft.csv', index=False)
-
-# 描述统计
-print('Python职位工资描述：\n{}'.format(df['月工资'].describe()))
-# 绘制频率直方图并保存
-plt.hist(df['月工资'], bins=12)
-plt.xlabel('工资 (千元)')
-plt.ylabel('频数')
-plt.title("工资直方图")
-plt.savefig('histogram.jpg')
-plt.show()
 
 
+def get_experience_histogram(df):
+    df['position_experience'] = df['work_year'].str.findall('\d+')
+    avg_work_year = []
+    for i in df['position_experience']:
+        # 如果工作经验为'不限'或'应届毕业生',那么匹配值为空,工作年限为0
+        if len(i) == 0:
+            avg_work_year.append(0)
+        # 如果匹配值为一个数值,那么返回该数值
+        elif len(i) == 1:
+            avg_work_year.append(int(''.join(i)))
+        # 如果匹配值为一个区间,那么取平均值
+        else:
+            num_list = list(range(int(i[0]), int(i[1]) + 1))
+            avg_work_year.extend(num_list)
+    # 绘制频率直方图并保存
+    plt.hist(avg_work_year, bins=10)  # bins的含义是分成几份
+    plt.xlabel('工作经验 (年)')
+    plt.ylabel('人数')
+    plt.title("工作经验需求直方图")
+    plt.savefig('工作经验需求直方图.jpg')
+    plt.show()
 
 
-# 绘制饼图并保存
-count = df['position_city'].value_counts()
-plt.pie(count, labels=count.keys(), labeldistance=1.1, autopct='%d%%')
-plt.axis('equal')  # 使饼图为正圆形
-plt.legend(loc='upper left', bbox_to_anchor=(-0.1, 1))
-plt.savefig('pie_chart_city.jpg')
-plt.show()
+def get_salary_histogram(df):
+    # 将字符串转化为列表,再取区间的前25%，比较贴近现实
+    df['salary'] = df['position_salary'].str.findall('\d+')
+    for k in df['salary']:
+        int_list = [int(n) for n in k]
+        try:
+            avg_wage = (int_list[0] + int_list[1]) / 2
+        except Exception as err:
+            print(err)
+            print(k)
+            avg_wage = int_list[0]
+        avg_salary.append(avg_wage)
+    df['月工资'] = avg_salary
+    # 将清洗后的数据保存,以便检查
+    df.to_csv('draft.csv', index=False)
+    # 描述统计
+    print('Python职位工资描述：\n{}'.format(df['月工资'].describe()))
+    # 绘制频率直方图并保存
+    plt.hist(df['月工资'], bins=10)  # bins的含义是分成几份
+    plt.xlabel('工资 (千元)')
+    plt.ylabel('人数')
+    plt.title("工资分布直方图")
+    plt.savefig('工资分布直方图.jpg')
+    plt.show()
 
-# 绘制饼图并保存
-count = df['position_area'].where(df['position_city'] == '北京').value_counts()
-plt.pie(count, labels=count.keys(), labeldistance=1.1, autopct='%2.1f%%')
-plt.axis('equal')  # 使饼图为正圆形
-plt.legend(loc='upper left', bbox_to_anchor=(-0.1, 1))
-plt.savefig('pie_chart_beijing.jpg')
-plt.show()
+
+def get_city_pie(df):
+    # 绘制饼图并保存
+    all_city = df['position_city'].value_counts()
+    series1 = all_city[all_city > 5]
+    series2 = pd.Series({'其他': all_city[all_city < 5].sum()})
+    result = pd.concat([series1, series2])
+    plt.pie(result, labels=result.keys(), labeldistance=1.1, autopct='%d%%')
+    plt.axis('equal')  # 使饼图为正圆形
+    plt.legend(loc='upper left', bbox_to_anchor=(-0.1, 1))
+    plt.savefig('工作城市占比饼图.jpg')
+    plt.show()
 
 
-# 绘制词云,将职位福利中的字符串汇总
-text = ''
-for line in df['position_desc']:
-    text += line
-    # 使用jieba模块将字符串分割为单词列表
-cut_text = ' '.join(jieba.cut(text))
-print(cut_text)
-color_mask = imread('cloud.jpg')  # 设置背景图
-cloud = WordCloud(
-    font_path='simhei.ttf',
-    background_color='white',
-    mask=color_mask,
-    max_words=1000,
-    max_font_size=100
-)
+def get_beijing_location_pie(df):
+    # 绘制饼图并保存
+    all_location = df['position_area'].where(df['position_city'] == '北京').value_counts()
+    series1 = all_location[all_location > 3]
+    series2 = pd.Series({'其他': all_location[all_location < 3].sum()})
+    result = pd.concat([series1, series2])
+    plt.pie(result, labels=result.keys(), labeldistance=1.1, autopct='%d%%')
+    plt.axis('equal')  # 使饼图为正圆形
+    plt.legend(loc='upper left', bbox_to_anchor=(-0.1, 1))
+    plt.savefig('北京工作地点占比饼图.jpg')
+    plt.show()
 
-word_cloud = cloud.generate(cut_text)
-# 保存词云图片
-word_cloud.to_file('word_cloud.jpg')
-plt.imshow(word_cloud)
-plt.axis('off')
-plt.show()
 
-# 实证统计,将学历不限的职位要求认定为最低学历:大专
-df['position_requ'] = df['position_requ'].replace('不限', '大专')
+def get_word_cloud(df):
+    text = ""
+    for line in df['position_desc']:
+        text += line
+    cut_text = ' '.join(jieba.cut(text))
+    print(cut_text)
+    color_mask = imread('cloud.jpg')  # 设置背景图
+    cloud = WordCloud(
+        font_path='simhei.ttf',
+        background_color='white',
+        mask=color_mask,
+        max_words=1000,
+        max_font_size=100
+    )
+    word_cloud = cloud.generate(cut_text)
+    # 保存词云图片
+    word_cloud.to_file('公司福利词云.jpg')
+    plt.imshow(word_cloud)
+    plt.axis('off')
+    plt.show()
 
-# 学历分为大专\本科\硕士,将它们设定为虚拟变量
-dummy_edu = pd.get_dummies(df['position_requ'], prefix='学历')
-print(dummy_edu)
-# 构建回归数组
-df_with_dummy = pd.concat([df['月工资'], df['经验'], dummy_edu], axis=1)
 
-# 建立多元回归模型
-y = df_with_dummy['月工资']
-X = df_with_dummy[['经验', '学历_大专', '学历_本科', '学历_硕士']]
-X = sm.add_constant(X)
-model = sm.OLS(y, X)
-results = model.fit()
-print('回归方程的参数：\n{}\n'.format(results.params))
-print('回归结果：\n{}'.format(results.summary()))
+if __name__ == "__main__":
+    print("read in csv data...")
+    # 读取数据
+    df = pd.read_csv('out.csv', encoding='gbk')
+    # 数据清洗,剔除实习岗位
+    df.drop(df[df['position_name'].str.contains('实习')].index, inplace=True)
+    print(df)
+    get_salary_histogram(df)
+    get_experience_histogram(df)
+    get_city_pie(df)
+    get_beijing_location_pie(df)
+    get_word_cloud(df)
+
+# def test(df):
+#     # 实证统计,将学历不限的职位要求认定为最低学历:大专
+#     df['education'] = df['education'].replace('不限', '大专')
+#
+#     # 学历分为大专\本科\硕士,将它们设定为虚拟变量
+#     dummy_edu = pd.get_dummies(df['education'], prefix='学历')
+#     #print(dummy_edu)
+#     # 构建回归数组
+#     df['salary'] = pd.Series(avg_salary)
+#     df_with_dummy = pd.concat([df['salary'], df['work_year'], dummy_edu], axis=1)
+#
+#     # 建立多元回归模型
+#     y = df_with_dummy['salary']
+#     X = df_with_dummy[['学历_大专', '学历_本科', '学历_硕士']]
+#     X = sm.add_constant(X)
+#     model = sm.OLS(y, X)
+#     results = model.fit()
+#     print('回归方程的参数：\n{}\n'.format(results.params))
+#     print('回归结果：\n{}'.format(results.summary()))
